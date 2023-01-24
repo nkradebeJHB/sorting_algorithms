427de68297cc669873f94c56ec1ca00d1713498a
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2023-01-24 15:39:48 -0800

    0

--- sort.h
+++ sort.h
@@ -2,6 +2,7 @@
 #define SORT_H
 
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -15,13 +16,12 @@ typedef struct listint_s
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
-/*Prototypes*/
+
+void bubble_sort(int *array, size_t size); 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-
 
 #endif
