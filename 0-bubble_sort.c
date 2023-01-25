ab53473de43bdaf09d37e31813c894600ee6908a
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2023-01-25 12:32:57 -0800

    0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,19 +8,19 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j, swap = 0;
+	size_t i, index, swap = 0;
 
 	if (array == NULL || size < 2)
 		return;
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size; j++)
+		for (index = 0; index < size; index++)
 		{
-			if (array[j] > array[j + 1] && array[j + 1])
+			if (array[index] > array[index + 1] && array[index + 1])
 			{
-				swap = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = swap;
+				swap = array[index];
+				array[index] = array[index + 1];
+				array[index + 1] = swap;
 				print_array(array, size);
 			}
 		}
---
+++ 2-selection_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * selection_sort - function that sorts an array of 
+ * integers in ascending order
+ *
+ * @array: array to sort
+ * @size: size of array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j;
+}
