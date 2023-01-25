b2450d8a94f7b1aa0527527afa4d91492903a9d7
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2023-01-25 12:19:34 -0800

    bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -16,7 +16,7 @@ void bubble_sort(int *array, size_t size)
 	{
 		for (j = 0; j < size; j++)
 		{
-			if (array[j + 1] && array[j] > array[j + 1])
+			if (array[j] > array[j + 1] && array[j + 1])
 			{
 				swap = array[j];
 				array[j] = array[j + 1];
