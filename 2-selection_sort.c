dee2fe202db63f3fc72a33ee3b48ae80ab54dfce
nkradebeJHB <nkradebe@student.wethinkcode.co.za>
2023-01-25 12:46:33 -0800

    2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,4 +10,26 @@
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j;
+	int swap, tmp, check = 0;
+
+	if (array == NULL || size < 2)
+		return;
+	for (i = 0; i <size; i++)
+	{
+		tmp = i;
+		check = 0;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[tmp] > array[j])
+			{
+				tmp = j;
+				check += 1;
+			}
+		}
+		swap = array[i];
+		array[i] = array[tmp];
+		array[tmp] = swap;
+		if (check != 0)
+			print_array(array, size);
+	}
 }
